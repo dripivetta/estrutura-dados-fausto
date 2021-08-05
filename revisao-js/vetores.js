5b3da67e235a1df0459351cb2007156bcc5ef409
dripivetta <dripivetta@hotmail.com>
2021-08-05 01:48:32 +0000

    Aula 02 Estrutura de Dados - Fausto

---
+++ revisao-js/vetores.js
@@ -0,0 +1,24 @@
+let frutas = ['maçã', 'laranja', 'pera', 'uva', 'mamão', 'banana']
+
+// Vetor original
+console.log(frutas)
+
+// Retirar o último elemento de um vetor
+let ultimaFruta = frutas.pop()
+
+// Valor alterado
+console.log(frutas)
+console.log(ultimaFruta)
+
+// Remoção do primeiro elemento do vetor
+let primeiraFruta = frutas.shift ()
+console.log(frutas)
+console.log(primeiraFruta)
+
+
+// Remoção de posição intermediária
+//1º parâmetro -> a posição da remoção (contagem começa no 0)
+//2º parâmetro -> a quantidade de elementos a remover
+let terceiraFruta = frutas.splice (2, 1) // o SPLICE sempre retornará um vetor 
+console.log(frutas)
+console.log(terceiraFruta)

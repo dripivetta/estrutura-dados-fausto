dee9f23f1ee1c808aea56392035c2aaf5fc68e92
dripivetta <dripivetta@hotmail.com>
2021-08-06 23:11:38 +0000

    Aula 04 Estrutura de Dados - Fausto

--- revisao-js/arrow-function.js
+++ revisao-js/arrow-function.js
@@ -16,3 +16,53 @@ console.log (quadrado(9))
 // c) Não são necessárias as chaves nem a palavra return
 const quadrado2 = n => n*n
 console.log (quadrado(9))
+
+// Função que tem mais de um parâmetro 
+function potencia (b, e){ // b= base, e= expoente
+    return b**e
+}
+console.log (potencia (2,8))
+
+// Função equivalente no formato arrow function
+let potencia2 = (b, e) => b**e
+// Quando tempos mais de um parâmetro, os parênteses em volta dos parâmetros em volta dos argumentos
+//voltam a ser obrigratório
+console.log (potencia(2,8))
+
+// Função sem argumento/parâmetro
+function megaSena (){
+    // Esta função retorna um número aleatório entre 1 e 60
+    // Math.random () -> retorna um número aleatório entre 0 (inclusive) e 1 (exclusive)
+    // * 60 -> ajusta a faixa de valores entre 0 e 59
+    // + 1 ajusta a faixa de valores para entre 1 e 60
+    // Math.floor -> arredondar para o menor número inteiro (tira as casas decimais)
+    return Math.floor(Math.random () * 60 + 1)
+}
+console.log (megaSena(), megaSena())
+
+// Função equivalente no formato arrow function
+// Quando a função não tem argumentos, na arrow function é necessário deixar um par de parênteses vazios
+// para marcar seu lugar
+const megaSena2 = () => Math.floor(Math.random() * 60 + 1)
+console.log (megaSena2 (), megaSena2 () )
+
+// Função com mais de uma linha de código no corpo
+function somaVetor (vet){
+    let soma = 0
+    for (let n of vet) soma+= n
+    return soma
+}
+console.log (somaVetor([12,42,-11,20,9,16]))
+
+// Função equivalente no formato arrow function
+// Quando a função tem mais de uma linha de código, na arrow function equivalente 
+// retornam as chaves e também a palavra return, caso ela retorne valor
+const somaVetor2 = vet => {
+    let soma = 0
+    for (let n of vet) soma+= n
+    return soma
+}
+console.log (somaVetor([12,42,-11,20,9,16]))
+
+// CONCLUSÃO: arrow function foram concebidas para facilitar a escrita de função de apenas
+// uma linha, embora também possam ser usadas para funções de múltiplas linhas.
---
+++ revisao-js/objetos.js
@@ -0,0 +1,60 @@
+// Objetos são estruturas que permitem armazenar diversos valores em uma variável,
+// permitindo acessá-los por meio de um nome
+
+// Criação de um objeto vazio
+let vazio1 = { } // forma "moderna"
+let vazio2 = new Object () // forma "traducional"
+
+// Criando um objeto já com dados
+let pessoa = {
+    // Os dados são armazenados no formato propriedade: valor
+    // ou também chamdo de atributo: valor
+    // Nome da propriedade pode ter espaços ou acentos, neste caso o nome da propriedade precisa estar entre aspas
+    nome: 'Orkutilson de Oliveira',
+    sexo: 'M',
+    dataNasc: '2008-04-19',
+    ocupacao: 'estudante',
+    gosto: ['jogos', 'doces', 'dormir'], // Vetor em uma posição de objeto
+    'cidade de origem': 'Franca/SP'
+}
+console.table (pessoa)
+
+// Exibindo apenas o valor de um propriedade
+console.log(pessoa.nome)
+console.log(pessoa.ocupacao)
+console.log(pessoa.gosto[1])
+// Quando o nome da propriedade tem espaços ou acentos, não é possível usar a sintaxe de ponto.
+// É necessário usar a sintaxe de colchetes
+console.log(pessoa['cidade de origem'])
+
+// A sintaxe de colchetes SEMPRE funciona, mesmo se o nome da propriedade for uma única palavra sem acento
+console.log(pessoa['nome'])
+console.log(pessoa['gosto'][1]) // Mostrando só o 2º gosto
+
+// Outra consequência da sintaxe do colchete é a possibilidade de usar nome de propriedades
+// que estão dentro de variáveis
+
+let x  ='sexo'
+console.log (pessoa[x])
+x = 'dataNasc'
+console.log (pessoa[x])
+
+// Criando uma nova propriedade para um objeto já existente
+// Basta atribuir um valor a um nome de propriedade ainda não existente
+pessoa.email ='orkutilson@gmail.com'
+pessoa.celular = '(16) 98765-4321'
+console.table (pessoa)
+
+// Criando um objeto vazio e só depois adicionando propriedades
+let carro = {}
+carro.marca = 'Volkswagen'
+carro.modelo = 'Combi'
+carro.ano = 1971
+carro['combustível'] = 'gasolina'
+carro.cor = 'bege'
+
+console.table (carro)
+
+// Excluindo uma propriedade de um objeto
+delete carro.ano
+console.table (carro)
